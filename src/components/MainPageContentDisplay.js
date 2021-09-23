14205668cd60e90949ddc6ff0f868e3e826578a1
Landry Geiger <landry.geiger@gmail.com>
2021-09-23 10:50:33 -0500

    Updated landing page text.

--- src/components/MainPageContentDisplay.js
+++ src/components/MainPageContentDisplay.js
@@ -19,7 +19,7 @@ const MainPageContent = () => {
             <Card.Body>
                 <h2>Competitive RPS Action</h2>
                 <div style={{display: "flex", justifyContent: "center"}}>
-                    <p style={{width: "100%", textAlign: "right"}}>filler text this is filler text i am filling up this space with text because i need it to look like there is actually something here please disregard what this actually says thank you.</p>
+                    <p style={{width: "100%", textAlign: "right"}}>An online Rock Paper Scissors Experience. Play with friends around the world and track your statistics.</p>
                 </div>
             </Card.Body>
         </Card>
